78c817d114a2966773146683a807d80610070f38
Gokul Kumar <gokulkumar0307@gmail.com>
2026-04-11 15:14:13 +0530

    terraform

---
+++ count_based_loop/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z0497875WFX6BJ1PL5O6" 
+}
+
+variable "domain_name" {
+    default = "gokul.shop" 
+}
